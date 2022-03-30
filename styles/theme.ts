33aae2feb815f8a247255137f28be55a03fabbe2
Dominik Patera <dominik.patera@gmail.com>
2022-03-30 20:13:08 +0200

    add: basic css variables

---
+++ styles/theme.ts
@@ -0,0 +1,26 @@
+export const media = {
+	sm: '576px',
+	md: '768px',
+	lg: '992px',
+	xl: '1200px',
+	xxl: '1400px',
+};
+
+export const colors = {
+	background: 'hsl(0, 0%, 0%)', // black
+	foreground: 'hsl(0, 0%, 100%)', // white
+	accents: [
+		'hsl(0, 0%, 7%)',
+		'hsl(0, 0%, 21%)',
+		'hsl(0, 0%, 28%)',
+		'hsl(0, 0%, 42%)',
+		'hsl(0, 0%, 56%)',
+		'hsl(0, 0%, 63%)',
+		'hsl(0, 0%, 92%)',
+	], // darkest -> lightest
+	error: {
+		light: 'hsl(0, 100%, 60%)',
+		normal: 'hsl(0, 100%, 50%)',
+		dark: 'hsl(0, 100%, 45%)',
+	},
+};
